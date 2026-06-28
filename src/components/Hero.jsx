f5c3092970bc32182ae4636a94ad60d934c76341
ShreyasGadave <shreyasgadave777@gmail.com>
2026-06-28 09:39:33 +0530

    Rate Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
             <button>Buy</button>
 
-            <p>From $1599 or $133/mo for 12 months</p>
+            <p>From ₹15999 or ₹1330/mo for 18 months</p>
         </section>
     )
 }
